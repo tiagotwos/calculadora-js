f0674806a9cde803cba44b333f1ef13970d8d579
tiagoteixeirabonifacio@gmail.com <67955976t>
2019-03-08 10:01:19 -0300

    'design'

---
+++ src/js/calculadora.js
@@ -0,0 +1,20 @@
+var Calculadora = {
+
+    adicionar: function(n1,n2){
+        return 0;
+    },
+    subtrair: function(n1,n2){
+        return 0;
+    },
+    dividir: function(n1,n2){
+        return 0;
+    },
+    multiplicar: function(n1,n2){
+        return 0;
+    }
+
+};
+
+if(typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
+    module.exports = Calculadora;
+}
